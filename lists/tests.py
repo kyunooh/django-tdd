bedce288e4b6cae338c6b72f13c8a83b0f9e25c7
kyunooh <chm073@gmail.com>
2015-11-05 22:32:07 +0900

    템플릿을 이용하도록 홈페이지 뷰를

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,9 @@
 from django.core.urlresolvers import resolve
+from django.template.loader import render_to_string
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
+
 
 class HomePageTest(TestCase):
 
@@ -12,6 +14,8 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+                                         
+
+        
