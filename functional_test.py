9d30a50a8a316eae684b283c74607d5e6518affd
kyunooh <chm073@gmail.com>
2015-11-14 16:13:14 +0900

    템플릿을 이용한 메인 페이지 HTML 생성

--- functional_test.py
+++ functional_test.py
@@ -37,6 +37,7 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: 공작깃털 사기' for row in rows),
+            "신규 작업이 테이블에 표시되지 않는다"
         )
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
