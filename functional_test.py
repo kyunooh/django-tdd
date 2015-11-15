fe4a75ec5980f31d3de3e5f9bfdb9e8a87a77581
kyunooh <chm073@gmail.com>
2015-11-15 12:47:05 +0900

    테이블 리스트 안에 해당 행이 있는지 확인하는 메소드 추가

--- functional_test.py
+++ functional_test.py
@@ -9,6 +9,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+        
     def test_cant_start_a_list_and_retrieve_it_later(self):
         #에디스(Edith)는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
         # 해당 웹 사이트를 확인하러 간다
@@ -28,11 +34,9 @@ class NewVisitorTest(unittest.TestCase):
         # (에디스의 취미는 날치 잡이용 그물을 만드는 것이다)
         inputbox.send_keys('공작깃털 사기')
         # 엔터키를 치면 페이지가 갱신되고 작업목록에
-        inputbox.send_keys(Keys.ENTER)
         # "1: 공작깃털 사기" 아이템이 추가된다
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])            
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
         # 다시 "공작깃털을이용해서 그물 만들기"라고 입력한다
@@ -42,13 +46,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
-        self.assertIn(
-            '2: 공작깃털을 이용해서 그물 만들기' ,
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
+
 
         # 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다
         # 사이트는 그녀를 위한 특정 URL을 생성해준다
