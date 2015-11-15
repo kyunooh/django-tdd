338558f78dd1ca8d1c19803b0a432afe8cc17755
kyunooh <chm073@gmail.com>
2015-11-15 20:45:35 +0900

    POST 후에 리다이렉셕과 템플릿에 모든 작업 아이템 표시

--- lists/tests.py
+++ lists/tests.py
@@ -24,14 +24,38 @@ class HomePageTest(TestCase):
 
         response = home_page(request)
 
-        self.assertIn('신규 작업 아이템', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': '신규 작업 아이템'}
-        )
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '신규 작업 아이템')
 
+    def test_home_page_redriects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
     
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
+
+    def test_home_page_dispalys_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+            
+            
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
@@ -51,5 +75,3 @@ class ItemModelTest(TestCase):
         self.assertEqual(first_saved_item.text, '첫 번째 아이템')
         self.assertEqual(second_saved_item.text, '두 번째 아이템')
 
-        
-    
