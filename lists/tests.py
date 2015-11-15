a73fde27111aa9a0b70d6b17b1678df984514d5f
kyunooh <chm073@gmail.com>
2015-11-15 12:34:30 +0900

    기능 테스트 리팩터링을 위한 준비

--- lists/tests.py
+++ lists/tests.py
@@ -16,6 +16,18 @@ class HomePageTest(TestCase):
         response = home_page(request)
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
-                                         
 
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': '신규 작업 아이템'}
+        )
+        self.assertEqual(response.content.decode(), expected_html)
         
