6aa80dfed29642ea85fdcd1652128284888e6f4a
kyunooh <chm073@gmail.com>
2015-11-16 15:00:48 +0900

    새로운 유저 시나리오 추가함

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -39,6 +39,8 @@ class NewVisitorTest(LiveServerTestCase):
         # 엔터키를 치면 페이지가 갱신되고 작업목록에
         # "1: 공작깃털 사기" 아이템이 추가된다
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: 공작깃털 사기')
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
@@ -52,11 +54,35 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
         self.check_for_row_in_list_table('1: 공작깃털 사기')
 
+        
+        
+        ## 새로운 브라우저 세션을 이용해서 에디스의 정보가
+        ## 쿠키를 통해 유입되는 것을 방지한다.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        # 프란시스가 홈페이지에 접속한다.
+        # 에디스의 리스트는 보이지 않는다
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('공작깃털 사기', page_text)
+        self.assertNotIn('그물 만들기', page_text)
+        
+        # 새로운 사용자인 프란시스가 사이트에 접속한다
+        # 그는 에디스보다 재미가 없다
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('우유 사기')
+        inputbox.send_keys(Keys.ENTER)
+        
+        # 프란시스가 전용 URL을 취득한다
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
 
-        # 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다
-        # 사이트는 그녀를 위한 특정 URL을 생성해준다
-        # 이때 URL에 대한 설명도 함께 제공된다
+        # 에디스가 입력한 흔적이 없다는 것을 다시 확인한다
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('공작깃털 사기', page_text)
+        self.assertIn('우유 사기', page_text)
+
+        # 둘 다 만족하고 잠자리에 든다
         self.fail('Finish the test!') 
-        # 해당 URL에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다
-        # 만족하고 잠자리에 든다
         browser.quit()
